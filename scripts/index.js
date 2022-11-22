ba32e52b110aabce28bef9a2f7bb55aeac4599d1
Denis Gabbasov <gabbas.denn@mail.ru>
2022-11-22 19:27:54 +0300

    feat:сверстать форму и добавить функционал добавления карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,86 @@
-const popupElement = document.querySelector('.popup');
+const popup = document.querySelectorAll('.popup');
+const popupElementEdit = document.querySelector('.popup_edit_form');
+const popupElementAdd = document.querySelector('.popup_add_form');
+
 const editButton = document.querySelector('.info__edit-button');
-const closePopupButton = popupElement.querySelector('.popup__close');
-const formElement = document.querySelector('.popup__form');
-const nameInput = formElement.querySelector('.popup__input_field_name');
-const postInput = formElement.querySelector('.popup__input_field_post');
+const addButton = document.querySelector('.info__add-button');
+const closePopupButton = document.querySelectorAll('.popup__close');
+
+const formElementEdit = document.querySelector('.popup__form_edit');
+const formElementAdd = document.querySelector('.popup__form_add');
+const nameInput = popupElementEdit.querySelector('.popup__input_field_name');
+const postInput = popupElementEdit.querySelector('.popup__input_field_post');
+const namePlaceInput = popupElementAdd.querySelector('.popup__input_field_place-name');
+const linkPlaceInput = popupElementAdd.querySelector('.popup__input_field_place-link');
+
+
+
 const nameProfile = document.querySelector('.info__profile-name');
 const postProfile = document.querySelector('.info__profile-post');
+
+
+
 const cardPlaceTemplate = document.querySelector('#card-place').content;
 const cardElemGridContainer = document.querySelector('.elements__grid-container');
 
-function openPopup() {
-  popupElement.classList.add('popup_opened');
+function openPopupEdit() {
+  popupElementEdit.classList.add('popup_opened');
 
   nameInput.value = nameProfile.textContent
   postInput.value = postProfile.textContent
 }
 
-function closePopup() {
-  popupElement.classList.remove('popup_opened');
+function openPopupAdd() {
+  popupElementAdd.classList.add('popup_opened');
 }
 
-function formSubmitHandler(evt) {
+function closePopup() {
+  popup.forEach(pop => {
+    pop.classList.remove('popup_opened');
+  });
+}
+
+function formSubmitHandlerAboutUser(evt) {
   evt.preventDefault();
 
   nameProfile.textContent = nameInput.value;
   postProfile.textContent = postInput.value;
 
-  closePopup()
+  closePopup();
 }
 
-editButton.addEventListener('click', openPopup)
-closePopupButton.addEventListener('click', closePopup)
-formElement.addEventListener('submit', formSubmitHandler);
+function formSubmitHandlerNewPlace(evt) {
+  evt.preventDefault();
+
+  let objCard = {
+    name: null,
+    link: null
+  }
+
+  objCard['name'] = namePlaceInput.value;
+  objCard['link'] = linkPlaceInput.value;
+
+  initialCards.unshift(objCard);
+
+  displayCards(initialCards);
+
+  namePlaceInput.value = '';
+  linkPlaceInput.value = '';
+
+  closePopup();
+
+}
+
+
+
+editButton.addEventListener('click', openPopupEdit);
+addButton.addEventListener('click', openPopupAdd);
+closePopupButton.forEach(btn => {
+  btn.addEventListener('click', closePopup);
+})
+formElementEdit.addEventListener('submit', formSubmitHandlerAboutUser);
+formElementAdd.addEventListener('submit', formSubmitHandlerNewPlace);
+// formElement.addEventListener('submit', formSubmitHandlerNewPlace);
 
 
 //Шесть карточек из коробки
@@ -62,18 +111,20 @@ const initialCards = [
   }
 ];
 
-
-
-function displayCardsFromArray() {
-  initialCards.forEach(item => {
-    const cardElement = cardPlaceTemplate.querySelector('.card-place').cloneNode(true);
-
-    cardElement.querySelector('.card-place__img').src = item.link;
-
-    cardElement.querySelector('.card-place__name').textContent = item.name;
-
-    cardElemGridContainer.append(cardElement);
-  });
+function displayCards(arr) {
+  cardElemGridContainer.innerHTML = '';
+  arr.forEach(createCard)
 }
 
-displayCardsFromArray();
+function createCard(card) {
+    const cardElement = cardPlaceTemplate.querySelector('.card-place').cloneNode(true);
+
+    cardElement.querySelector('.card-place__img').src = card.link;
+
+    cardElement.querySelector('.card-place__name').textContent = card.name;
+
+    cardElemGridContainer.append(cardElement);
+}
+
+
+displayCards(initialCards);
