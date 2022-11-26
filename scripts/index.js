ab352c670dd1599ff321f6abfd7fbbf0619b2891
Denis Gabbasov <gabbas.denn@mail.ru>
2022-11-26 09:39:53 +0300

    feat: отделить обработчики данных от данных

--- scripts/index.js
+++ scripts/index.js
@@ -91,33 +91,6 @@ popupImageCloseButton.addEventListener('click', () => closePopup(popupElementZoo
 
 
 //Шесть карточек из коробки
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
 function displayCards(arr) {
   arr.forEach((item) => {
     renderCard(item, cardElemGridContainer);
