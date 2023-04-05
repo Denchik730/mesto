575cdd7366c5478f7bf83fd85883cdf6cbff36f6
Denis Gabbasov <gabbas.denn@mail.ru>
2023-04-05 17:12:19 +0300

    fix:перенести нахождение элементов попапа картинки с метода constructor в метод  open

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,17 +3,14 @@ import Popup from "./Popup.js";
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
-    this._popupImage = this._popup.querySelector('.popup__place-image');
-    this._popupDescr = this._popup.querySelector('.popup__place-descr');
   }
 
-  open(popupZoomData) {
+  open({ name, link }) {
     super.open();
-
-    this._popupImage.src = popupZoomData.link;
-    this._popupImage.alt = `Изображение ${popupZoomData.name}`;
-    this._popupDescr.textContent = popupZoomData.name;
-
+    this._popupImage = this._popup.querySelector(".popup__place-image");
+    this._popupDescr = this._popup.querySelector(".popup__place-descr");
+    this._popupImage.src = link;
+    this._popupImage.alt = `Изображение ${name}`;
+    this._popupDescr.textContent = name;
   }
-
 }
